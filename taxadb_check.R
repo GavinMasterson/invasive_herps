7b8c39cc0ca9b062017c377484b4dce65b3814b1
GavinMasterson <gavinmasterson@gmail.com>
2020-03-16 14:18:14 +0200

    updated with 'verbatim code' format and kableExtra tables

--- taxadb_check.R
+++ taxadb_check.R
@@ -1,20 +1,22 @@
+library(tidyverse)
+library(dplyr)
 library(taxadb)
 library(dplyr)
 
-td_create("iucn")
+GISD_query <- read_delim("amrep_gisd.csv", trim_ws = TRUE, delim = ";") %>%
+  .[,-8] %>% 
+  separate(Species, c("Genus", "Specific_Epithet", "Subspecific_Epithet"), sep = " ", remove = FALSE) 
+
+td_create("itis")
 database <- filter_rank(c("Amphibia", "Reptilia"), "class")
 
-db_check <- sp_list %>% 
-                left_join(database, by = c("Species" = "scientificName")) %>%
-                mutate(Genus = genus.x,
-                       SpecificEpithet = species,
-                       InfraSpecificEpithet = subspecies,
-                       ) %>% 
-                select(species_GISD = Species,
-                       vernacularName_ITIS = vernacularName,
-                       order_GISD = Order, order_ITIS = order,
-                       family_GISD = Family, family_ITIS = family,
-                       taxonomicStatus_ITIS = taxonomicStatus)
+db_check <- GISD_query %>% 
+  left_join(database, by = c("Species" = "scientificName")) %>%
+  select(species_GISD = Species,
+         vernacularName_ITIS = vernacularName,
+         order_GISD = Order, order_ITIS = order,
+         family_GISD = Family, family_ITIS = family,
+         taxonomicStatus_ITIS = taxonomicStatus)
 
 
 synonym <- db_check[which(db_check$taxonomicStatus_ITIS == "synonym"),]
